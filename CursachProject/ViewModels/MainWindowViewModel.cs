a8c02f94714f7487ca329219894e021f2df75c1a
Papa Lisa <ishakgang@catdog.cc>
2024-03-12 12:09:22 +0300

    Добавьте файлы проекта.

---
+++ CursachProject/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursachProject.ViewModels
+{
+    internal class MainWindowViewModel
+    {
+    }
+}
